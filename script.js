233a4cb2a8d8e78652c6e26256568aa60db0476c
Maureen Oldyck <maureen.oldyck@gmail.com>
2020-12-08 15:00:04 +0100

    Switch Function for weather text and animations

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 //TODO: Function for chart
-//TODO: Function for weather icon text
 //TODO: Function for weather icon
 //TODO: Function for timezone
 //TODO: Function for accordions
 //TODO: Function for chart
-
-
 
 
 
@@ -18,7 +15,54 @@
     const content = document.querySelector(".collapsButton").nextElementSibling;
     let canvas = document.querySelector(".temp-info").nextElementSibling;
     const weatherText = document.querySelector(".weather-image").nextElementSibling;
+    let  currentWeather;
     let chart;
+
+
+
+
+
+    const displayWeatherTextAndAnimation = () => {
+
+        switch (currentWeather) {
+            case "Clouds":
+                document.querySelector(".weather-text").innerHTML = "";
+                document.querySelector("img").classList.add("cloud");
+                document.querySelector(".weather-text").innerHTML = "Hello there, it's a cloudy day today. Eventhough the sun might not be shining at the moment, you will always be my sunshine!";
+                break;
+
+            case "Rain":
+                document.querySelector(".weather-text").innerHTML = "";
+                document.querySelector("img").classList.add("snow");
+                document.querySelector(".weather-text").innerHTML = "Hello there, it's snowing! The perfect moment to cosy up with a loved one, a thick blanket and hot chocolate!"
+
+
+            case "Snow":
+                document.querySelector(".weather-text").innerHTML = "";
+                document.querySelector("img").classList.add("rain");
+                document.querySelector("img").style.paddingBottom = "10px";
+                document.querySelector(".weather-text").innerHTML = "Hello there, it's a rainy day today. If you don't like rain, think about the fact that without rain there would be no life. So get out there and enjoy the rain! Don't forget your umbrella!"
+                break;
+
+            case "Clear":
+                document.querySelector(".weather-text").innerHTML = "";
+                document.querySelector("img").classList.add("sun");
+                document.querySelector("img").style.paddingBottom = "20px";
+                document.querySelector(".weather-text").innerHTML = "Hello there, the sun is shining, how nice! Definitely the perfect time to go out and get those Vitamin D! Don't forget to put on sunscreen!";
+                break;
+
+            default:
+                document.querySelector(".weather-text").innerHTML = "";
+                document.querySelector("img").classList.add("cloud");
+                document.querySelector(".weather-text").innerHTML = "Hello there, make the best out of today. I believe in you!"
+                break;
+        }
+    };
+
+
+
+
+
 
     // Default value based on current location
     window.addEventListener("load", () => {
@@ -72,6 +116,7 @@
             .then(
                 ((response) => {
                     response.json().then((weatherInfo => {
+
                         document.querySelector(".temperature").innerHTML = Math.round(weatherInfo.main.temp) + "°C";
                         document.querySelector(".city-name").innerHTML = weatherInfo.name + ", " + weatherInfo.sys.country;
                         document.querySelector(".description").innerHTML = weatherInfo.weather[0].main;
@@ -80,29 +125,10 @@
                         document.querySelector(".weather-image").src = "images/" + weatherInfo.weather[0].main.toLowerCase() + ".png";
 
                         // Script for animations (add class for specific description) & for the weather texts
-                        if (weatherInfo.weather[0].main == "Clouds") {
-                            document.querySelector(".weather-text").innerHTML = "";
-                            document.querySelector("img").classList.add("cloud");
-                            document.querySelector(".weather-text").innerHTML = "Hello there, it's a cloudy day today. Eventhough the sun might not be shining at the moment, you will always be my sunshine!";
-                        } else if (weatherInfo.weather[0].main == "Clear") {
-                            document.querySelector(".weather-text").innerHTML = "";
-                            document.querySelector("img").classList.add("sun");
-                            document.querySelector("img").style.paddingBottom = "20px";
-                            document.querySelector(".weather-text").innerHTML = "Hello there, the sun is shining, how nice! Definitely the perfect time to go out and get those Vitamin D! Don't forget to put on sunscreen!";
-                        } else if (weatherInfo.weather[0].main == "Rain") {
-                            document.querySelector(".weather-text").innerHTML = "";
-                            document.querySelector("img").classList.add("rain");
-                            document.querySelector("img").style.paddingBottom = "10px";
-                            document.querySelector(".weather-text").innerHTML = "Hello there, it's a rainy day today. If you don't like rain, think about the fact that without rain there would be no life. So get out there and enjoy the rain! Don't forget your umbrella!"
-                        } else if (weatherInfo.weather[0].main == "Snow") {
-                            document.querySelector(".weather-text").innerHTML = "";
-                            document.querySelector("img").classList.add("snow");
-                            document.querySelector(".weather-text").innerHTML = "Hello there, it's snowing! The perfect moment to cosy up with a loved one, a thick blanket and hot chocolate!"
-                        } else {
-                            document.querySelector(".weather-text").innerHTML = "";
-                            document.querySelector("img").classList.add("cloud");
-                            document.querySelector(".weather-text").innerHTML = "Hello there, make the best out of today. I believe in you!"
-                        }
+                        currentWeather = weatherInfo.weather[0].main;
+                        displayWeatherTextAndAnimation();
+
+
 
                         // Get current time of location 
                         let currentTimeOffset = (new Date().getTimezoneOffset()) / 60; // Offset in hours
@@ -191,7 +217,7 @@
                                     pointBorderWidth: 1,
                                     pointHoverRadius: 5,
                                     pointHitRadius: 10,
-                                }, { 
+                                }, {
                                     label: "Temperature in °C",
                                     fill: false,
                                     borderColor: "rgba(75, 192, 192, 1)",
@@ -219,7 +245,7 @@
                                     yAxes: [{
                                         display: false,
                                     }],
-                                    ticks: [{ 
+                                    ticks: [{
                                         beginAtZero: true,
                                     }]
                                 },
