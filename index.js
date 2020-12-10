e74b10ee79465a8d521b91a7a4f322d7890ba27b
Maureen Oldyck <maureen.oldyck@gmail.com>
2020-12-10 11:37:07 +0100

    Add locationFunction module

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-//TODO: Function for weather icon
     
     import { displayWeatherTextAndAnimation } from "./src/displayWeatherTextAndAnimation.js";
     import { accordionFunction } from "./src/accordion.js";
     import { backgroundColorChange } from "./src/backgroundColor.js";
     import { createChart } from "./src/createChart.js";
     import { emptyArray } from "./src/emptyArray.js";
+    import { locationFunction } from "./locationFunction.js";
     
 
     // Declaration of variables 
@@ -29,33 +29,7 @@
                 let latitude = position.coords.latitude;
                 let api = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=current,hourly,minutely&units=metric&appid=be4553b34e49d94c654cc1c6eb775c17";
 
-                fetch(api)
-                    .then((response) => {
-                        response.json().then((locationInfo => {
-                            document.querySelector(".temperature").innerHTML = Math.round(locationInfo.daily[0].temp.day) + "°C";
-                            document.querySelector(".city-name").innerHTML = "Current Location";
-                            document.querySelector(".description").innerHTML = locationInfo.daily[0].weather[0].main;
-                            document.querySelector(".highest").innerHTML = "<i class='fas fa-caret-up'></i>" + Math.round(locationInfo.daily[0].temp.max) + "°C";
-                            document.querySelector(".lowest").innerHTML = "<i class='fas fa-caret-down'></i>" + Math.round(locationInfo.daily[0].temp.min) + "°C";
-                            document.querySelector(".weather-image").src = "./images/" + locationInfo.daily[0].weather[0].main.toLowerCase() + ".png";
-                            document.querySelector(".tomorrow").innerHTML = "Tomorrow";
-                            document.querySelector(".tomorrowTemperature").innerHTML = Math.round(locationInfo.daily[1].temp.day) + "°C";
-                            document.querySelector(".weather-icon-tomorrow").src = "./images/" + locationInfo.daily[1].weather[0].main.toLowerCase() + ".png";
-                            document.querySelector(".dayAfterTomorrow").innerHTML = "In Two Days";
-                            document.querySelector(".dayAfterTomorrowTemperature").innerHTML = Math.round(locationInfo.daily[2].temp.day) + "°C";
-                            document.querySelector(".weather-icon-dayAfterTomorrow").src = "./images/" + locationInfo.daily[2].weather[0].main.toLowerCase() + ".png";
-                            document.querySelector(".inThreeDays").innerHTML = "In Three Days";
-                            document.querySelector(".inThreeDaysTemperature").innerHTML = Math.round(locationInfo.daily[3].temp.day) + "°C";
-                            document.querySelector(".weather-icon-inThreeDays").src = "./images/" + locationInfo.daily[3].weather[0].main.toLowerCase() + ".png";
-                            document.querySelector(".inFourDays").innerHTML = "In Four Days";
-                            document.querySelector(".inFourDaysTemperature").innerHTML = Math.round(locationInfo.daily[4].temp.day) + "°C";
-                            document.querySelector(".weather-icon-inFourDays").src = "./images/" + locationInfo.daily[4].weather[0].main.toLowerCase() + ".png";
-                            document.querySelector(".inFiveDays").innerHTML = "In Five Days";
-                            document.querySelector(".inFiveDaysTemperature").innerHTML = Math.round(locationInfo.daily[5].temp.day) + "°C";
-                            document.querySelector(".weather-icon-inFiveDays").src = "./images/" + locationInfo.daily[5].weather[0].main.toLowerCase() + ".png";
-
-                        }));
-                    });;
+                locationFunction(api);
             });
         };
 
@@ -83,7 +57,6 @@
 
                         currentWeather = weatherInfo.weather[0].main;
                         displayWeatherTextAndAnimation(currentWeather);
-
 
                         // Get current time of location 
                         let currentTimeOffset = (new Date().getTimezoneOffset()) / 60; // Offset in hours
