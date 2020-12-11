68acba1d3336fbdcf25de126e0d24d1492382705
Maureen Oldyck <maureen.oldyck@gmail.com>
2020-12-11 11:01:24 +0100

    Use then statement for json response

--- src/index.js
+++ src/index.js
@@ -42,40 +42,44 @@ window.addEventListener("load", () => {
 
     // Weather Today / Now
     fetch(weatherToday)
-        .then(
-            ((response) => {
-                response.json().then((weatherInfo => {
+        .then((response) => {
 
-                    weatherInformation(weatherInfo);
-                    backgroundColorChange(weatherInfo);
+            return response.json();
+        
+        })
+        .then((weatherInfo => {
 
-                    currentWeather = weatherInfo.weather[0].main;
-                    displayWeatherTextAndAnimation(currentWeather);
+            weatherInformation(weatherInfo);
+            backgroundColorChange(weatherInfo);
+
+            currentWeather = weatherInfo.weather[0].main;
+            displayWeatherTextAndAnimation(currentWeather);
                                      
-                }))
-            }));
+        }))
 
     // Weather next 5 days 
     fetch(weatherForecast)
-        .then(
-            ((response) => {
-                response.json().then((forecastInfo => {
+        .then((response) => {
+
+            return response.json()
+            
+        })
+        .then((forecastInfo => {
                     
-                    emptyArray(dayArray);
-                    forecastArray(forecastInfo, dayArray);
-                    forecastInformation(dayArray);
-                    emptyArray(hourArray);
-                    emptyArray(tempArray);
-                    emptyArray(precipitationArray);
-                    chartInfo(forecastInfo, hourArray, tempArray, precipitationArray );
-                    createChart(temperatureChart, precipitationArray, tempArray, hourArray);
+            emptyArray(dayArray);
+            forecastArray(forecastInfo, dayArray);
+            forecastInformation(dayArray);
+            emptyArray(hourArray);
+            emptyArray(tempArray);
+            emptyArray(precipitationArray);
+            chartInfo(forecastInfo, hourArray, tempArray, precipitationArray );
+            createChart(temperatureChart, precipitationArray, tempArray, hourArray);
                     
-                }));
-            })
-        );;
+        }));
 
     // Make search bar disappear again after search 
     content.style.maxHeight = 0;
+    
 };
 
 document.querySelector("#run").addEventListener("click", searchClicked);
