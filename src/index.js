e50864f92523fa1a2f0b952f0ee0f354b02a3411
Maureen Oldyck <maureen.oldyck@gmail.com>
2020-12-11 10:50:14 +0100

    Make seperate function searchClicked, to call when button is clicked

--- src/index.js
+++ src/index.js
@@ -33,49 +33,52 @@ import _ from 'lodash';
         };
     });
 
-    document.querySelector("#run").addEventListener("click", (event) => {
-        event.preventDefault();
-        const cityName = document.querySelector("#city").value;
-        const weatherToday = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=metric&appid=be4553b34e49d94c654cc1c6eb775c17";
-        const weatherForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=metric&appid=be4553b34e49d94c654cc1c6eb775c17";
+    const searchClicked = (event) => {
 
-        // Weather Today / Now
-        fetch(weatherToday)
-            .then(
-                ((response) => {
-                    response.json().then((weatherInfo => {
+    event.preventDefault();
+    const cityName = document.querySelector("#city").value;
+    const weatherToday = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=metric&appid=be4553b34e49d94c654cc1c6eb775c17";
+    const weatherForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=metric&appid=be4553b34e49d94c654cc1c6eb775c17";
 
-                        weatherInformation(weatherInfo);
-                        backgroundColorChange(weatherInfo);
+    // Weather Today / Now
+    fetch(weatherToday)
+        .then(
+            ((response) => {
+                response.json().then((weatherInfo => {
 
-                        currentWeather = weatherInfo.weather[0].main;
-                        displayWeatherTextAndAnimation(currentWeather);
-                                         
-                    }))
+                    weatherInformation(weatherInfo);
+                    backgroundColorChange(weatherInfo);
+
+                    currentWeather = weatherInfo.weather[0].main;
+                    displayWeatherTextAndAnimation(currentWeather);
+                                     
+                }))
+            }));
+
+    // Weather next 5 days 
+    fetch(weatherForecast)
+        .then(
+            ((response) => {
+                response.json().then((forecastInfo => {
+                    
+                    emptyArray(dayArray);
+                    forecastArray(forecastInfo, dayArray);
+                    forecastInformation(dayArray);
+                    emptyArray(hourArray);
+                    emptyArray(tempArray);
+                    emptyArray(precipitationArray);
+                    chartInfo(forecastInfo, hourArray, tempArray, precipitationArray );
+                    createChart(temperatureChart, precipitationArray, tempArray, hourArray);
+                    
                 }));
+            })
+        );;
 
-        // Weather next 5 days 
-        fetch(weatherForecast)
-            .then(
-                ((response) => {
-                    response.json().then((forecastInfo => {
-                        
-                        emptyArray(dayArray);
-                        forecastArray(forecastInfo, dayArray);
-                        forecastInformation(dayArray);
-                        emptyArray(hourArray);
-                        emptyArray(tempArray);
-                        emptyArray(precipitationArray);
-                        chartInfo(forecastInfo, hourArray, tempArray, precipitationArray );
-                        createChart(temperatureChart, precipitationArray, tempArray, hourArray);
-                        
-                    }));
-                })
-            );;
+    // Make search bar disappear again after search 
+    content.style.maxHeight = 0;
+};
 
-        // Make search bar disappear again after search 
-        content.style.maxHeight = 0;
-    });
+    document.querySelector("#run").addEventListener("click", searchClicked);
 
     accordionFunction(".collapsButton", content);
     accordionFunction(".weather-image", weatherText);
